17a2d6171591e2633873272807c1d0c3c23d6385
Edith Mendoza Barraza <edith.mendoza@neoris.com>
2019-10-14 16:52:46 -0600

    InitializeTestProject

--- BlogPostControllerTests.cs
+++ BlogPostControllerTests.cs
@@ -8,20 +8,33 @@ namespace WebAPIStarter.Tests
     public class BlogPostControllerTests
     {
         [Fact]
-        public void GetAll_WhenCalled_ReturnsOKResult()
+        public void GetById_WhenCalledWithExistingId_ReturnsOKResult()
         {
             //Arrange
             BlogPostController blogPostController = new BlogPostController();
 
             //Act
-            var getResult = blogPostController.GetAllPosts();
+            var getResult = blogPostController.GetById(1);
 
             //Assert
             Assert.IsType<OkObjectResult>(getResult);
         }
 
         [Fact]
-        public void Create_WhenCalled_WithValidBlogPost_ReturnsCreatedActionResult()
+        public void GetById_WhenCalledWithNonExistingId_ReturnsNoContent()
+        {
+            //Arrange
+            BlogPostController blogPostController = new BlogPostController();
+
+            //Act
+            var getResult = blogPostController.GetById(4);
+
+            //Assert
+            Assert.IsType<NoContentResult>(getResult);
+        }
+
+        [Fact]
+        public void Create_WhenCalled_WithValidBlogPost_ReturnsStatusCodeResult()
         {
             //Given
             BlogPostController blogPostController = new BlogPostController();
@@ -34,8 +47,75 @@ namespace WebAPIStarter.Tests
 
             //When
             var getResult = blogPostController.InsertNewPost(post);
-
-            Assert.IsType<CreatedAtActionResult>(getResult);
+            
+            //Then
+            var result = Assert.IsType<StatusCodeResult>(getResult);
+            Assert.Equal(201, result.StatusCode);
         }
+
+        [Fact]
+        public void Delete_WhenCalledWithExistingId_ReturnsStatusCode() {
+            //Given
+            BlogPostController blogPostController = new BlogPostController();
+           
+            //When
+            var getResult = blogPostController.Delete(1);
+
+            //Then
+            var result = Assert.IsType<StatusCodeResult>(getResult);
+            Assert.Equal(410, result.StatusCode);
+        }
+
+        [Fact]
+        public void Delete_WhenCalledWithNonExistingId_ReturnsNotFound() {
+            //Given
+            BlogPostController blogPostController = new BlogPostController();
+           
+            //When
+            var getResult = blogPostController.Delete(4);
+
+            //Then
+            Assert.IsType<NotFoundResult>(getResult);
+        }
+
+        [Fact]
+        public void Put_WhenUpdatedAPost_ReturnsStatusCode() {
+            //Given
+            BlogPostController blogPostController = new BlogPostController();
+            PostModel post = new PostModel
+            {
+                Id = 1,
+                Title = "Blog Post",
+                Author = "Roberto",
+                Content = "Fourth Blog Post by Roberto"
+            };
+
+            //When
+            var getResult = blogPostController.Put(post);
+
+            //Then
+            Assert.IsType<OkResult>(getResult);
+        }
+
+
+
+
+        // [Fact]
+        // public void Create_WhenCalled_WithInValidBlogPost_ReturnsStatusCodeResult()
+        // {
+        //     //Given
+        //     BlogPostController blogPostController = new BlogPostController();
+        //     PostModel newpost = new PostModel
+        //     {
+        //         Content = "Fourth Blog Post by Roberto"
+        //     };
+
+        //     //When
+        //     var getResult = blogPostController.InsertNewPost(newpost);
+
+        //     //Then
+        //     var result = Assert.IsType<StatusCodeResult>(getResult);
+        //     Assert.Equal(400, result.StatusCode);
+        // }
     }
 }
